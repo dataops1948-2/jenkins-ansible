a4326f2ef700ca8db818ff5955f3cdf446eb0d83
RepUser <RepUser@Rep-2240.Repsol.com>
2020-08-05 16:40:01 +0300

    checkout

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -14,7 +14,7 @@ pipeline {
         }
         stage('Deploy') {
             steps {
-                scm
+                scm checkout
                 echo 'Deploying....'
                 script{
                 // ansiblePlaybook credentialsId: 'private_key', inventory: 'inventories/a/hosts', playbook: 'my_playbook.yml's
