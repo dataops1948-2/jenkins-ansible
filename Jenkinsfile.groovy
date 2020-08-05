4bb46362ab6c2062faf34f6b07dd79a79f5d98aa
RepUser <RepUser@Rep-2240.Repsol.com>
2020-08-05 14:58:36 +0300

    a

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,7 +16,8 @@ pipeline {
             steps {
                 echo 'Deploying....'
                 // ansiblePlaybook credentialsId: 'private_key', inventory: 'inventories/a/hosts', playbook: 'my_playbook.yml's
-                ansiblePlaybook credentialsId: 'private_key' playbook: 'my_playbook.yml'
+                // ansiblePlaybook credentialsId: 'private_key' playbook: 'my_playbook.yml'
+                ansiblePlaybook(credentialsId: 'private_key', inventory: 'inventories/a/hosts', playbook: 'my_playbook.yml')
             }
         }
     }
